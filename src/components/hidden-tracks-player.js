2c896db1827ca6f27c80df219531d9dd3f067534
Jeffrey Baucom <kishi@Jeffreys-MBP.attlocal.net>
2021-04-12 17:58:09 -0400

    replace bandcamp src

--- src/components/hidden-tracks-player.js
+++ src/components/hidden-tracks-player.js
@@ -3,10 +3,13 @@ export default function HiddenTracksPlayer() {
     return (
         <div>
             <iframe 
-                style={{border: "0", width: "100%", height: "120px"}}
-                src=""
-                seamless>
-                <a href=""></a>
+            style={{border: "0", width: "400px", height: "638px"}}
+            src="https://bandcamp.com/EmbeddedPlayer/album=977298969/size=large/bgcol=ffffff/linkcol=0687f5/transparent=true/tracklist=true/tracks=1835300261,3446136580,1076005904,1361087763/esig=ce4f9d4b142c71956c6b3620acb69cf5/" 
+            seamless
+            >
+                <a href="https://curatedbydfr.bandcamp.com/album/secret-recipe">
+                Secret Recipe by DFR
+            </a>
             </iframe>
         </div>
     )
